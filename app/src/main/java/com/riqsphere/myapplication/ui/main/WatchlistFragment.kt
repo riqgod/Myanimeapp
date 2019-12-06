a2df1a348e7e7701faf5a1cf134d0680d1578121
Henrique <ham2@cin.ufpe.br>
2019-12-06 18:05:47 -0300

    WatchlistFragment.kt and WAtchlistAnimeAdapter.kt updated to inflate recycler view but need bugfix

--- app/src/main/java/com/riqsphere/myapplication/ui/main/WatchlistFragment.kt
+++ app/src/main/java/com/riqsphere/myapplication/ui/main/WatchlistFragment.kt
@@ -1,6 +1,7 @@
 package com.riqsphere.myapplication.ui.main
 
 import android.content.Context
+import android.graphics.ColorSpace
 import android.net.Uri
 import android.os.Bundle
 import android.util.Log
@@ -8,19 +9,56 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.github.doomsdayrs.jikan4java.core.Connector
+import com.github.doomsdayrs.jikan4java.types.main.anime.Anime
 
 import com.riqsphere.myapplication.R
+import com.riqsphere.myapplication.watchlist.room.WatchlistAnime
+import com.riqsphere.myapplication.watchlist.room.WatchlistAnimeAdapter
 import com.riqsphere.myapplication.watchlist.room.WatchlistViewModel
 
 
 class WatchlistFragment : Fragment() {
+
+    private lateinit var recyclerView:RecyclerView
+    private lateinit var viewAdapter: WatchlistAnimeAdapter
+    private lateinit var viewManager: RecyclerView.LayoutManager
+    private lateinit var watchlistViewModel: WatchlistViewModel
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        Log.d("debugg","watchhhh")
-        return inflater.inflate(R.layout.fragment_watchlist, container, false)
+
+        watchlistViewModel = WatchlistViewModel(this.activity!!.application)
+        val temporaryAnime = WatchlistAnime(temporaryFetch(20))
+        watchlistViewModel.insert(temporaryAnime) //naruto
+
+        val view = inflater.inflate(R.layout.fragment_watchlist,container,false)
+        val watchlistAdapter = WatchlistAnimeAdapter(activity!!.applicationContext)
+
+
+        viewManager = GridLayoutManager(activity,3)
+        viewAdapter = watchlistAdapter
+        recyclerView = view.findViewById<RecyclerView>(R.id.wl_rv).apply {
+            layoutManager = viewManager
+            setHasFixedSize(true)
+            adapter = viewAdapter
+            addItemDecoration(DividerItemDecoration(activity, DividerItemDecoration.VERTICAL))
+        }
+
+        return view
     }
+
+
+    private fun temporaryFetch(id:Int):Anime{
+        val anime = Connector().retrieveAnime(id).get()
+        return anime
+    }
+
 }
--- app/src/main/java/com/riqsphere/myapplication/watchlist/room/WatchlistAnimeAdapter.kt
+++ app/src/main/java/com/riqsphere/myapplication/watchlist/room/WatchlistAnimeAdapter.kt
@@ -5,27 +5,45 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.riqsphere.myapplication.R
+import com.riqsphere.myapplication.utils.ImageHandler
+import kotlinx.android.synthetic.main.watchlist_card.view.*
 
-class WatchlistAnimeAdapter internal constructor (context: Context) : RecyclerView.Adapter<WatchlistAnimeAdapter.WatchlistAnimeViewHolder>() {
+class WatchlistAnimeAdapter (private val context: Context) : RecyclerView.Adapter<WatchlistAnimeAdapter.WatchlistAnimeViewHolder>() {
     private val inflater = LayoutInflater.from(context)
     private var watchlistAnimes = emptyList<WatchlistAnime>()
 
-    inner class WatchlistAnimeViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+    inner class WatchlistAnimeViewHolder(itemView: View):
+        RecyclerView.ViewHolder(itemView) {
         @SuppressLint("ResourceType")
-        val wordItemView: TextView = itemView.findViewById(1)
+       private val cardAnimeImage: ImageView = itemView.findViewById(R.id.wl_image)
+        private val cardAnimeTitle :TextView = itemView.findViewById(R.id.wl_anime_title)
+        private val cardAnimeProgress: ProgressBar = itemView.findViewById(R.id.wl_progress_bar)
+
+        fun bindView(wlCard: WatchlistAnime){
+            ImageHandler.getInstance(this@WatchlistAnimeAdapter.context).load(wlCard.imgURL).into(cardAnimeImage)
+            cardAnimeTitle.text = wlCard.title
+            cardAnimeProgress.max = wlCard.episodes
+            cardAnimeProgress.progress = wlCard.episodesOut // just a placeholder.
+        }
+
     }
 
     @SuppressLint("ResourceType")
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): WatchlistAnimeViewHolder {
-        val itemView = inflater.inflate(123, parent, false)
+        val itemView =
+            LayoutInflater.from(parent.context).inflate(R.layout.watchlist_card,parent,false)
         return WatchlistAnimeViewHolder(itemView)
     }
 
     override fun onBindViewHolder(holder: WatchlistAnimeViewHolder, position: Int) {
         val current = watchlistAnimes[position]
         //manipulate holder using values from current
+        holder.bindView(current)
     }
 
     internal fun setWatchlistAnime(watchlistAnimes: List<WatchlistAnime>) {
