1446144692622ea0a0dcd6df9fbb44a8fce7a413
cubsphere <6753972+cubsphere@users.noreply.github.com>
2019-12-15 22:37:30 -0300

    attempt to fix some memory leaks

--- app/src/main/java/com/riqsphere/myapplication/ui/animeDetail/AnimeDetailActivity.kt
+++ app/src/main/java/com/riqsphere/myapplication/ui/animeDetail/AnimeDetailActivity.kt
@@ -1,9 +1,7 @@
 package com.riqsphere.myapplication.ui.animeDetail
 
 import android.os.AsyncTask
-import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
@@ -12,7 +10,6 @@ import com.github.doomsdayrs.jikan4java.types.main.anime.Anime
 import com.google.android.material.tabs.TabLayout
 import com.riqsphere.myapplication.R
 import com.riqsphere.myapplication.cache.JikanCacheHandler
-import com.riqsphere.myapplication.room.MyaaViewModel
 import com.riqsphere.myapplication.utils.ImageHandler
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.runBlocking
@@ -20,79 +17,64 @@ import kotlinx.coroutines.withContext
 
 class AnimeDetailActivity : AppCompatActivity() {
 
-    private lateinit var myaaViewModel: MyaaViewModel
-
-    private var id = -1
-    private lateinit var fragmentPagerAdapter: AnimeDetailFragmentPagerAdapter
-
-    private lateinit var partialAnime: Anime
-
-    private lateinit var viewPager: ViewPager
-    private lateinit var tabs: TabLayout
-    private lateinit var imageBg: ImageView
-    private lateinit var animeTitle: TextView
-    private lateinit var animeSubtitle: TextView
-    private lateinit var animeScore: TextView
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.anime_detail_activity)
 
-        id = intent.getIntExtra("id", -1)
+        val id = intent.getIntExtra("id", -1)
         if (id == -1) {
             return
         }
 
-        myaaViewModel = MyaaViewModel(application)
-        partialAnime = Anime().apply { mal_id = id }
+        val partialAnime = Anime().apply { mal_id = id }
 
-        fragmentPagerAdapter = AnimeDetailFragmentPagerAdapter(
+        val fragmentPagerAdapter = AnimeDetailFragmentPagerAdapter(
             this,
             partialAnime,
             supportFragmentManager
         )
-        viewPager = findViewById(R.id.anime_view_pager)
+        val viewPager = findViewById<ViewPager>(R.id.anime_view_pager)
         viewPager.adapter = fragmentPagerAdapter
-        tabs = findViewById(R.id.tabs_anime)
+        val tabs = findViewById<TabLayout>(R.id.tabs_anime)
         tabs.setupWithViewPager(viewPager)
 
         //setting the content
-        imageBg = findViewById(R.id.anime_bg_collapsing_image)
-        animeTitle = findViewById(R.id.anime_bg_title)
-        animeSubtitle = findViewById(R.id.anime_bg_subtitle)
-        animeScore = findViewById(R.id.anime_bg_score)
+        val imageBg = findViewById<ImageView>(R.id.anime_bg_collapsing_image)
+        val animeTitle = findViewById<TextView>(R.id.anime_bg_title)
+        val animeSubtitle = findViewById<TextView>(R.id.anime_bg_subtitle)
+        val animeScore = findViewById<TextView>(R.id.anime_bg_score)
 
-        fetchAnime()
-        fetchVideo()
-        fetchRecs()
+        fetchAnime(fragmentPagerAdapter, id, imageBg, animeTitle, animeSubtitle, animeScore)
+        fetchVideo(fragmentPagerAdapter, partialAnime)
+        fetchRecs(fragmentPagerAdapter, partialAnime)
     }
 
-    private fun fetchAnime() {
+    private fun fetchAnime(fpa: AnimeDetailFragmentPagerAdapter, id: Int, imageBg: ImageView, animeTitle: TextView, animeSubtitle: TextView, animeScore: TextView) {
         AsyncFetcherSetter({
             JikanCacheHandler.getAnime(id)
         }, {
-            setActivityData(it)
-            fragmentPagerAdapter.setAnime(it)
+            setActivityData(it, imageBg, animeTitle, animeSubtitle, animeScore)
+            fpa.setAnime(it)
         }).execute()
     }
 
-    private fun fetchVideo() {
+    private fun fetchVideo(fpa: AnimeDetailFragmentPagerAdapter, partialAnime: Anime) {
         AsyncFetcherSetter({
             JikanCacheHandler.getVideo(partialAnime)
         }, {
-            fragmentPagerAdapter.setEpisodes(it)
+            fpa.setEpisodes(it)
         }).execute()
     }
 
-    private fun fetchRecs() {
+    private fun fetchRecs(fpa: AnimeDetailFragmentPagerAdapter, partialAnime: Anime) {
         AsyncFetcherSetter({
             JikanCacheHandler.getRecommendationPage(partialAnime)
         }, {
-            fragmentPagerAdapter.setRecs(it)
+            fpa.setRecs(it)
         }).execute()
     }
 
-    private fun setActivityData(anime: Anime) {
+    private fun setActivityData(anime: Anime, imageBg: ImageView, animeTitle: TextView, animeSubtitle: TextView, animeScore: TextView) {
         ImageHandler.getInstance(this).load(R.drawable.dororo).into(imageBg)
         animeTitle.text = anime.title
         animeSubtitle.text = getGenres(anime)
